52a57c47014173939458742fb7d1d2dbc005dd3a
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-11-28 20:10:21 -0500

    react-hooks-react-router-code-along-v6
    react-hooks-react-router-code-along-v6

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About () {
+    return (
+        <>
+          <header>
+        {/*{/" Save space for Navbar*/}
+    </header>
+    <main>
+        <h1>This is my about componet!</h1>
+    </main>
+    </>
+    );
+};
+
+export default About;
